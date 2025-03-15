5ddb68320f4dafcb4aaea80229c75f09c3b86ba0
LaHieu <lahieutx@gmail.com>
2025-03-15 14:48:01 +0700

    SCRUM-86 finish

--- back_end/src/routes/product.routes.js
+++ back_end/src/routes/product.routes.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { productController } = require('../controller')
-
+const VerifyJwt = require('../middlewares/verifyJwt');
 
 const ProductRouter = express.Router()
 ProductRouter.use(bodyParser.json())
 
+// Public routes
 ProductRouter.get("/", productController.getAllProducts);
-ProductRouter.get("/:id", productController.getProductById);
-ProductRouter.post("/create", productController.createProduct);
-ProductRouter.put("/edit/:id", productController.updateProduct);
-ProductRouter.delete("/delete/:id", productController.deleteProduct);
-ProductRouter.get("/products/user/:userId", productController.getProductsByUserId);
+ProductRouter.get("/shop/:shopId", productController.getProductsByShopId); // Route for getting products by shop ID
+ProductRouter.get("/user/:userId", productController.getProductsByUserId);
+ProductRouter.get("/:id", productController.getProductById); // This must come after other specific routes
 
+// Protected routes - requires authentication
+ProductRouter.post("/create", [VerifyJwt.verifyToken], productController.createProduct);
+ProductRouter.put("/edit/:id", [VerifyJwt.verifyToken], productController.updateProduct);
+ProductRouter.delete("/delete/:id", [VerifyJwt.verifyToken], productController.deleteProduct);
 
 module.exports = ProductRouter
